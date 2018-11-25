dbf09591dab4247f433e0262210a9b95fed9c017
Evgeniy Volohovich <evgeniyvolohovich@gmail.com>
2018-11-25 21:31:33 +0700

    Trie : print  -> GetAllSequences

--- Trie/main.cpp
+++ Trie/main.cpp
@@ -16,10 +16,13 @@ public:
 		Insert(std::begin(list), std::end(list));
 	}
 
-	void Print() const
+	std::vector<std::vector<T>> GetAllSequences() const
 	{
-		std::vector<T> v;
-		Print(v);
+		std::vector<std::vector<T>> allSequences;
+		std::vector<T> curSequences;
+		GetAllSequences(curSequences, allSequences);
+
+		return allSequences;
 	}
 
 	std::optional<std::reference_wrapper<const Trie>> Subtrie(const std::initializer_list<T>& list) const
@@ -50,21 +53,19 @@ private:
 		return found->second.Subtrie(std::next(itBegin), itEnd);
 	}
 
-	void Print(std::vector<T>& v) const
+	void GetAllSequences(std::vector<T>& curSequences, std::vector<std::vector<T>>& allSequences) const
 	{
 		if (m_tries.empty())
-		{
-			std::copy(std::begin(v), std::end(v), std::ostream_iterator<T>{std::cout, " "});
-			std::cout << std::endl;
-		}
+			allSequences.push_back(curSequences);
 
 		for (const auto& p : m_tries)
 		{
-			v.push_back(p.first);
-			p.second.Print(v);
-			v.pop_back();
+			curSequences.push_back(p.first);
+			p.second.GetAllSequences(curSequences, allSequences);
+			curSequences.pop_back();
 		}
 	}
+
 
 private:
 	std::map<T, Trie> m_tries;
@@ -75,10 +76,21 @@ int main()
 	Trie<std::string> t;
 
 	t.Insert({"hi", "how", "are", "you"});
-	t.Insert({"hi", "i", "am", "great", "thanks"});
 	t.Insert({"what", "are", "you", "doing"});
 	t.Insert({"i", "am", "watching", "a", "movie"});
+	t.Insert({"hi", "i", "am", "great", "thanks"});
 
-	if (auto st (t.Subtrie({"hi"})); st)
-		st->get().Print();
+	const auto subTrie = t.Subtrie({"hi"});
+
+	if (subTrie)
+	{
+		const auto allSequences = subTrie->get().GetAllSequences();
+		for (const auto& sequences : allSequences)
+		{
+			std::copy(std::cbegin(sequences), std::cend(sequences), std::ostream_iterator<std::string>{std::cout, " "});
+			std::cout << std::endl;
+		}
+	}
+
+	return 0;
 }
